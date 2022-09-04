21238302fa469bee96919d1281803cff2679710a
godebam247 <112554019+godebam247@users.noreply.github.com>
2022-09-04 15:06:22 +0300

    zadacha

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 10;
+        double s = 0.15;
+        int b = 20;
+        int o = 10;
+        int d = 2;
+        System.out.println(x*o);
+        int x2 = 100;
+        System.out.println(x2*s);
+        int s2 = 15;
+        System.out.println(b*d);
+        int b2 = 40;
+        System.out.println(x2-s2+b2);
+
+    }
+}
